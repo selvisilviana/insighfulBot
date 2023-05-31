ad473f4962a1fdaf5936072d1bce6be27c102af3
selvisilviana <101259902+selvisilviana@users.noreply.github.com>
2023-05-31 19:53:00 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,98 +1,54 @@
-# =[Modules dan Packages]========================
-
-from flask import Flask,render_template,request,jsonify
-from flask_ngrok import run_with_ngrok
-import pandas as pd
+from flask import Flask, render_template, request, jsonify
 import numpy as np
-import re
-import pickle
-import nltk
-import joblib
-import pickle
-from joblib import load
+from tensorflow.keras.models import load_model
+from flask_ngrok import run_with_ngrok
 
-#import package untuk phishing detection
-from nltk.tokenize import RegexpTokenizer
-from nltk.stem.snowball import SnowballStemmer
-from sklearn.feature_extraction.text import CountVectorizer
-
-#import package untuk safebot
-from process import preparation, generate_response
-
-# download nltk
-preparation()
-
-# =[Variabel Global]=============================
-
-app   = Flask(__name__, static_url_path='/static')
+app = Flask(__name__)
 model = None
 
-# Text Pre-Processing function untuk API phishing detection
-def text_preprocessing_process(text):
-    tokens = tokenizer.tokenize(text)
-    tokens_stemmed = [stemmer.stem(token) for token in tokens]
-    processed_text = ' '.join(tokens_stemmed)
-    return processed_text
-
-# =[Routing]=====================================
+# Load model LSTM
+def load_lstm_model():
+    global model
+    model = load_model('chatbot_model.h5')
+    # Optional: Lakukan persiapan lain yang diperlukan seperti pengaturan tokenizer, dll.
 
 # [Routing untuk Halaman Utama atau Home]	
 @app.route("/")
 def beranda():
     return render_template('index.html')
 
-# [Routing untuk Halaman Utama atau Home]	
-@app.route("/chatbot")
-def chatbot():
-    return render_template('chatbot.html')
-
-# Routing for API phishing
+# [Routing untuk API]
 @app.route("/api/deteksi", methods=['POST'])
-def apiDeteksi():
-    prediction_input = ""
-    
+def api_deteksi():
     if request.method == 'POST':
-        prediction_input = request.form['data']
+        # Ambil input dari pengguna
+        input_text = request.form['input_text']
         
-        # Text Pre-Processing
-        prediction_input = text_preprocessing_process(prediction_input)
+        # Lakukan preprocessing pada input_text jika diperlukan
         
-        # Vectorization
-        feature = cv.transform([prediction_input])
+        # Lakukan prediksi menggunakan model LSTM
+        prediksi = lstm_predict(input_text)
         
-        # Prediction (Web Phishing or Web Aman)
-        prediction = model.predict(feature)
-        
-        if prediction == 0:
-            hasil_prediksi = "Web Phishing"
-        else:
-            hasil_prediksi = "Web Aman"
-        
-        # Return the prediction result in JSON format
-        return jsonify({
-            "data": hasil_prediksi,
-        })
+        # Return hasil prediksi dengan format JSON
+        return jsonify({"prediksi": prediksi})
+
+# Prediksi menggunakan model LSTM
+def lstm_predict(input_text):
+    # Lakukan transformasi pada input_text jika diperlukan
     
-# Routing for API response chatbot
-@app.route("/get")
-def get_bot_response():
-    user_input = str(request.args.get('msg'))
-    result = generate_response(user_input)
-    return result
+    # Lakukan prediksi menggunakan model LSTM
+    # Contoh:
+    # input_data = transform_input_text(input_text)
+    # prediksi = model.predict(input_data)
+    
+    # Lakukan post-processing pada hasil prediksi jika diperlukan
+    
+    return prediksi
 
-# =[Main]========================================
-
+# Main
 if __name__ == '__main__':
-        
-    #Setup Phishing
-	tokenizer = RegexpTokenizer(r'[A-Za-z]+')
-	stemmer = SnowballStemmer("english")
-   	
-	cv = CountVectorizer(vocabulary=pickle.load(open('feature.pickle', 'rb')))
-	
-	# Load model phishing yang telah ditraining
-	model = load('chatbot_model.h5')
 
-	# Run Flask di Google Colab menggunakan ngrok
-	run_with_ngrok(app)
-	app.run()
+
+    # Run Flask di localhost 
+      run_with_ngrok(app)
+      app.run()
